9be9e996a5070bd04bf0617c775ae90539d85b56
timothychilezi20 <167911886+timothychilezi20@users.noreply.github.com>
2025-10-29 20:20:49 +0200

    Adding a Music Page

---
+++ Music/music.js
@@ -0,0 +1,8 @@
+const tabs = document.querySelectorAll(".tab");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    tabs.forEach((t) => t.classList.remove("active"));
+    tab.classList.add("active");
+  });
+});
